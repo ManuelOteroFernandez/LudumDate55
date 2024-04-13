7b5480a252ff9528df63f9c8cb2251ce45951109
Manuel Otero <manuelOtero.af@gmail.com>
2024-04-13 21:24:38 +0200

    [FIX] Win condition

--- global.gd
+++ global.gd
@@ -12,5 +12,5 @@ const _runesToWin = 6
 var _runesCollected = 0
 func collect_rune():
 	_runesCollected += 1
-	if _runesCollected <= _runesToWin:
+	if _runesCollected >= _runesToWin:
 		get_tree().change_scene_to_file("win_screen.tscn")
