469c1d29f5290a1d87a557aec864ced78155b8b6
Manuel Otero <manuelOtero.af@gmail.com>
2024-04-13 13:15:57 +0200

    Collision with enemies

--- Character/Character.gd
+++ Character/Character.gd
@@ -3,29 +3,51 @@ class_name Character extends CharacterBody2D
 signal change_life(newValue:int)
 
 @export var limits = Vector4(0,0,1100,600)
-@export var lifes = 5
+@export var lifes = 4
+
 const SPEED = 12000.0
 
-func get_damage():
+var unmove = false
+
+func receive_damage(repulsionVelocity:Vector2):
+	velocity = repulsionVelocity
+	unmove = true
+	$HitTimer.start()
 	lifes -= 1
 	change_life.emit(lifes)
+	print(lifes)
 	
 func _physics_process(delta):
-	var directionX = Input.get_axis("ui_left", "ui_right")
-	
-	if limits.x < position.x and directionX < 0: 
-		velocity.x = directionX * SPEED * delta
-	elif position.x < limits.z and directionX > 0:
-		velocity.x = directionX * SPEED * delta
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if not unmove:
+		var directionX = Input.get_axis("ui_left", "ui_right")
+		if directionX < 0: 
+			velocity.x = directionX * SPEED * delta
+		elif directionX > 0:
+			velocity.x = directionX * SPEED * delta
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	var directionY = Input.get_axis("ui_up", "ui_down")
-	if limits.y < position.y and directionY < 0: 
-		velocity.y = directionY * SPEED * delta
-	elif position.y < limits.z and directionY > 0:
-		velocity.y = directionY * SPEED * delta
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		var directionY = Input.get_axis("ui_up", "ui_down")
+		if directionY < 0: 
+			velocity.y = directionY * SPEED * delta
+		elif directionY > 0:
+			velocity.y = directionY * SPEED * delta
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	# Block movement out of limits
+	if limits.x > position.x and velocity.x < 0: 
+		velocity.x = 0
+	elif position.x > limits.z and velocity.x > 0:
+		velocity.x = 0
+
+	if limits.y > position.y and velocity.y < 0: 
+		velocity.y = 0
+	elif position.y > limits.w and velocity.y > 0:
+		velocity.y = 0
 		
 	move_and_slide()
+
+
+func _on_hit_timer_timeout():
+	unmove = false
--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -5,7 +5,13 @@ const SPEED = 10000.0
 func _physics_process(delta):
 	velocity.y = SPEED * delta
 	move_and_slide()
-
+	if get_slide_collision_count() > 0:
+		var collision = get_slide_collision(0)
+		velocity.x = collision.get_normal().x * SPEED * 2 * delta
+		var collider = collision.get_collider()
+		if collider.has_method("receive_damage"):
+			collider.receive_damage(collision.get_normal() * SPEED * 2 * delta * -1)
+		move_and_slide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
