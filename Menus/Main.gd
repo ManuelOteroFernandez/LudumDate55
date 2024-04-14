b69dcc3d8facd736e456b0aadd429d667f89e5fb
Manuel Otero <manuelOtero.af@gmail.com>
2024-04-14 09:02:04 +0200

    Change main scene and name of game scene

--- Menus/Main.gd
+++ Menus/Main.gd
@@ -5,8 +5,8 @@ var emisor
 func _ready():
 	emisor = $Pivote
 
-func _process(delta):
+func _process(_delta):
 	emisor.rotation += 0.002
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
