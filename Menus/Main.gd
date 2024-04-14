b496d74dca423f66b345d23abe7b6e1bd7880220
x0tero <gxoelotero@gmail.com>
2024-04-14 06:23:41 +0200

    menu finiquitado

--- Menus/Main.gd
+++ Menus/Main.gd
@@ -6,7 +6,7 @@ func _ready():
 	emisor = $Pivote
 
 func _process(delta):
-	emisor.rotation += 0.004
+	emisor.rotation += 0.002
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
