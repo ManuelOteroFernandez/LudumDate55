8cf4f9a08a8bb3f8316e1cb8f888ac96ef822166
Manuel Otero <manuelOtero.af@gmail.com>
2024-04-15 22:01:39 +0200

    [FIX] Button start press many times

--- Menus/Main.gd
+++ Menus/Main.gd
@@ -4,21 +4,20 @@ var emisor
 @onready var audioS = $botonSonHover
 @onready var audioI = $botonSonClick
 
-#@onready var simultaneous_scene = preload("res://Menus/TransitionScreen.tscn").instantiate()
+var _menu_exited = false
 signal exit_menu
 func _ready():
 	emisor = $Pivote
-	#$TransitionScreen.hide()
 
 func _process(_delta):
 	emisor.rotation += 0.002
 
 func _on_button_pressed():
-	#get_tree().root.add_child(simultaneous_scene)
-	emit_signal("exit_menu")
-	#get_tree().change_scene_to_file("res://Menus/Transition.tscn")
-	audioI.play(0.0)
+	if not _menu_exited:
+		_menu_exited = true
+		emit_signal("exit_menu")
+		audioI.play()
 
 
 func _on_button_mouse_entered():
-	audioS.play(0.0)
+	audioS.play()
