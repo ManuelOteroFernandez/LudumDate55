b15214c49a6d65f268852dc16aff051b669713cd
Manuel Otero <manuelOtero.af@gmail.com>
2024-04-15 16:26:17 +0200

    Better script

--- server.py
+++ server.py
@@ -1,17 +1,50 @@
 from http import server
+import os
+import socket
 
 class MyRequestHandler (server.SimpleHTTPRequestHandler):
 
     def end_headers (self):
-
         self.send_header('Access-Control-Allow-Origin', '*')
-
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
 
         server.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
+    import argparse
+    import contextlib
 
-    server.test(MyRequestHandler)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cgi', action='store_true',
+                        help='run as CGI server')
+    parser.add_argument('--bind', '-b', metavar='ADDRESS',
+                        help='specify alternate bind address '
+                             '(default: all interfaces)')
+    parser.add_argument('--directory', '-d', default=os.getcwd(),
+                        help='specify alternate directory '
+                             '(default: current directory)')
+    parser.add_argument('port', action='store', default=8000, type=int,
+                        nargs='?',
+                        help='specify alternate port (default: 8000)')
+    args = parser.parse_args()
+    
+    class DualStackServer(server.ThreadingHTTPServer):
+
+        def server_bind(self):
+            # suppress exception when protocol is IPv4
+            with contextlib.suppress(Exception):
+                self.socket.setsockopt(
+                    socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+            return super().server_bind()
+
+        def finish_request(self, request, client_address):
+            self.RequestHandlerClass(request, client_address, self,
+                                     directory=args.directory)
+
+    server.test(
+        HandlerClass=MyRequestHandler,
+        ServerClass=DualStackServer,
+        port=args.port,
+        bind=args.bind,
+    )
